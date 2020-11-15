18cedc0d9f8d5af4a7a3eb9f8d3fca89fdb5314f
erika618 <68714247+erika618@users.noreply.github.com>
2020-11-15 17:08:37 +0900

    index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '29074'
+    end
+  end    
 end
