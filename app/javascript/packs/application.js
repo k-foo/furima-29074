7cdc7565dbb655ea69b24d4d6b17c37bb9fd05c4
erika618 <68714247+erika618@users.noreply.github.com>
2020-11-15 13:44:48 +0900

    turbolinksの無効化（Gem）
    画面遷移を高速化するGemだが、JavaScriptのイベント発火が阻害されるため、コメントアウトにて無効化。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,8 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// JavaScriptイベント発火をするために、画面遷移高速化のGemを無効化する。
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
