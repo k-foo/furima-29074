ce4f45d8646a4895ca1dd51dbcf4481d66ee85fb
erika618 <68714247+erika618@users.noreply.github.com>
2020-11-22 20:19:52 +0900

    userモデルにバリデーションを実装
    deviseのデフォルトの機能と重複しないようにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # has_many :items
+  # has_many :buys
+  # has_many :comments
+
+  # 存在すること・確認用を含めて2回入力・6字以上はdeviseのデフォルト実装のため省略
+  # 半角英数字（空文字NG）以外の場合には、メッセージを出す
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  validates :nickname, presence: true, uniqueness: true
+  # @含むこと・存在することはdeviseのデフォルト実装のため省略
+  validates :email,    uniqueness: true
+  # 全角ひらがな、全角カタカナ、漢字
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  # 全角カタカナ
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
 end
