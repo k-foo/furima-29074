6058ed7403213f7e9c281df5b97a95c923d8f3b0
erika618 <68714247+erika618@users.noreply.github.com>
2020-11-24 20:16:13 +0900

    itemモデルの単体テストコードの修正。（空欄もNGとする）
    ActiveHashについて、選択肢が「０」だと保存できないのと同じように「nil」であっても保存できないことをテストコードとして実装。合わせて’’をnilに修正。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -47,12 +47,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it '商品名が空欄だと出品できない' do
-        @item.name = ''
+        @item.name = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it '商品の説明が空欄だと出品できない' do
-        @item.description = ''
+        @item.description = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
@@ -61,28 +61,53 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Category must be other than 0')
       end
+      it 'カテゴリーの情報が空欄だと出品できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank", 'Category is not a number')
+      end
       it '商品の状態の情報が「---」だと出品できない' do
         @item.item_status_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include('Item status must be other than 0')
+      end
+      it '商品の状態の情報が空欄だと出品できない' do
+        @item.item_status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status can't be blank", 'Item status is not a number')
       end
       it '配送料の負担の情報が「---」だと出品できない' do
         @item.shipping_cost_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping cost must be other than 0')
       end
+      it '配送料の負担の情報が空欄だと出品できない' do
+        @item.shipping_cost_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank", 'Shipping cost is not a number')
+      end
       it '発送元の地域の情報が「---」だと出品できない' do
         @item.delivery_area_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include('Delivery area must be other than 0')
+      end
+      it '発送元の地域の情報が空欄だと出品できない' do
+        @item.delivery_area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery area can't be blank", 'Delivery area is not a number')
       end
       it '発送までの日数の情報が「---」だと出品できない' do
         @item.shipping_date_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping date must be other than 0')
       end
+      it '発送までの日数の情報が空欄だと出品できない' do
+        @item.shipping_date_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank", 'Shipping date is not a number')
+      end
       it '価格が空欄だと出品できない' do
-        @item.price = ''
+        @item.price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
       end
