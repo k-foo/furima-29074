5cfb747f6c33d976c60d9102ab20aec612e23222
erika618 <68714247+erika618@users.noreply.github.com>
2020-11-24 19:02:32 +0900

    itemモデルの単体テストコードの修正。（user_idを必須とする）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -36,6 +36,11 @@ RSpec.describe Item, type: :model do
     end
 
     context '出品ができないとき' do
+      it 'ユーザー登録している人でないと出品できない' do
+        @item.user_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist', "User can't be blank")
+      end
       it '１枚画像がないと出品できない' do
         @item.image = nil
         @item.valid?
