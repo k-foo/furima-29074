ccf5c967f9a79357560cac6b44364d3a6d669461
erika618 <68714247+erika618@users.noreply.github.com>
2020-11-26 18:28:51 +0900

    トップページから商品をクリックして詳細ページへ行くよう実装。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはトップページに促す
   before_action :authenticate_user!, except: :index
+  # 重複処理をまとめる
+  before_action :set_item, only: :show
+  # before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,9 +22,29 @@ class ItemsController < ApplicationController
     end
   end
 
+  # def destroy
+  #   item = Item.find(params[:id])
+  #   item.destroy
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  #   item = Item.find(params[:id])
+  #   item.update(item_params)
+  # end
+
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :delivery_area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
