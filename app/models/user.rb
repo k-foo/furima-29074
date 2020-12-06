413233ee2f7adfb33122c6a5427ef578e00fecec
erika618 <68714247+erika618@users.noreply.github.com>
2020-12-06 09:36:54 +0900

    ２つのテーブル作成に伴いuserテーブルのアソシエーション修正。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  # has_many :buys
+  has_many :orders
   # has_many :comments
 
   with_options presence: true do
