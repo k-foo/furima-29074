1594f9c39187679bd892e7e12af0d7b217c012e6
erika618 <68714247+erika618@users.noreply.github.com>
2021-02-07 17:35:42 +0900

    config/deploy.rbの編集。

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,33 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.15.0"
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.15.0'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# Capistranoのログの表示に利用する
+set :application, 'furima-29074'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:erika618/furima-29074.git'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/sde234grwwsfcfxgb.pem'] 
 
-# Default value for :pty is false
-# set :pty, true
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
